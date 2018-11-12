d124c6562e5fe7421d0a22c25630809ce6339a90
quyenphamkhac <pkquyen@sk-global.biz>
2018-11-12 23:43:25 +0700

    fix message date time format

--- src/components/molecules/MessageData.js
+++ src/components/molecules/MessageData.js
@@ -2,16 +2,18 @@ import React from 'react';
 
 import Icon from '../atoms/Icon';
 
+import moment from 'moment';
+
 const messageData = (props) => (
   <div className={`message-data ${props.isMyMessage ? "" : "align-right"}`}>
     {props.isMyMessage ? (
       <React.Fragment>
         <span className="message-data-name"><Icon icon="fa fa-circle online" /> {props.sender}</span>
-        <span className="message-data-time">{props.timestamp}</span>
+        <span className="message-data-time">{moment(props.timestamp).fromNow()}</span>
       </React.Fragment>
     ) : (
       <React.Fragment>
-        <span className="message-data-time" >{props.timestamp}</span> &nbsp; &nbsp;
+        <span className="message-data-time" >{moment(props.timestamp).fromNow()}</span> &nbsp; &nbsp;
         <span className="message-data-name" >{props.sender}</span> <Icon icon="fa fa-circle me" />
       </React.Fragment>
     )}
