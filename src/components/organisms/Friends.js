30108d4436b7fe2df8e3e18820216d26fe8291e4
quyenphamkhac <pkquyen@sk-global.biz>
2018-11-12 23:27:16 +0700

    sort friend list by condition

--- src/components/organisms/Friends.js
+++ src/components/organisms/Friends.js
@@ -14,10 +14,36 @@ const getLastOnlineTime = (uid, sessions) => {
 }
 
 const friends = (props) => {
-  const { data, presence, sessions } = props;
+  const { data, presence, sessions, stars, auth } = props;
   const onlines = !isLoaded(presence) ? null : presence;
   const userSessions = !isLoaded(sessions) ? null : sessions;
-  const friendList = userSessions && onlines && data && Object.keys(data).map((key, id) => (
+  const me = !isLoaded(auth) ? null : auth;
+  const favorites = !isLoaded(stars) ? null : stars;
+
+  //sort list user by condition
+  //1. my stars list
+  let myStars = favorites && me && Object.keys(favorites[me.uid]).filter(key => {
+    return favorites[me.uid][key] === true;
+  });
+  myStars = myStars ? myStars : [];
+
+  //2. onlines list
+  let myOnlFriends = onlines && Object.keys(onlines);
+  myOnlFriends = myOnlFriends ? myOnlFriends : [];
+
+  //3. merge stars and online list
+  let highPriority = myStars && myOnlFriends && Array.from(new Set([...myStars, ...myOnlFriends]));
+
+  //4. get low priority user and sort by alphabet
+  let lowPriority = me && data && highPriority && Object.keys(data)
+    .filter(user => !highPriority.includes(user) && user !== me.uid)
+    .sort((u1, u2) => (data[u1].displayName > data[u2].displayName));
+  lowPriority = lowPriority ? lowPriority : [];
+
+  //5. merge low and high
+  let friends = [...highPriority, ...lowPriority];
+
+  const friendList = userSessions && onlines && friends && data && me && friends.filter(user => user !== me.uid).map((key) => (
       <Friend 
         key={key} 
         uid={key} 
@@ -40,9 +66,12 @@ export default compose(
   firebaseConnect([
     'presence',
     'sessions',
+    'stars',
   ]),
-  connect(({ firebase: { data } }) => ({ 
+  connect(({ firebase: { data, auth } }) => ({ 
     presence: data.presence,
     sessions: data.sessions,
+    stars: data.stars,
+    auth,
   }))
 )(friends);
